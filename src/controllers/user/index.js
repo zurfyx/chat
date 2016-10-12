db4ffcd39f94e09ffbd0c7d5579ee5a256da4e3d
zurfyx <zurfyx@gmail.com>
2016-10-12 19:39:22 +0200

    whoami will return 401 if not signed in

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -2,5 +2,7 @@ import User from '~/models/User';
 
 // Current logged in user.
 export const getWhoami = (req, res, next) => {
-  res.json(req.user || {});
+  return (req.user)
+    ? res.json(req.user)
+    : res.status(401).json({ error: 'Not signed in' });
 };
