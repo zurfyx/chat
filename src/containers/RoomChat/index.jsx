d35a3dffd6437cfbc152248cab0f3df44fef84a1
zurfyx <zurfyx@gmail.com>
2016-12-03 12:40:29 +0100

    separated chat box into a different component

--- src/containers/RoomChat/index.jsx
+++ src/containers/RoomChat/index.jsx
@@ -1,43 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { send as sendMessage } from 'redux/modules/message';
 import RoomChatHistory from 'containers/RoomChatHistory';
+import RoomChatMessage from 'containers/RoomChatMessage';
 
 export class RoomChat extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      formContent: '',
-    };
-
-    this.handleFormKeyDown = this.handleFormKeyDown.bind(this);
-    this.handleFormContentChange = this.handleFormContentChange.bind(this);
-    this.handleSendTextMessage = this.handleSendTextMessage.bind(this);
-  }
-
-  handleFormKeyDown(e) {
-    // Form can be submitted through the input message textarea too!
-    // That is by using the Enter key (Shift-Enter for new lines).
-    if (e.keyCode === 13 && !e.shiftKey) {
-      this.handleSendTextMessage(e);
-    }
-  }
-
-  handleFormContentChange(e) {
-    this.setState({ formContent: e.target.value });
-  }
-
-  handleSendTextMessage(e) {
-    e.preventDefault();
-
-    if (this.state.formContent !== '') {
-      this.props.sendMessage(this.props.chat._id, this.state.formContent, 'plain');
-      this.setState({formContent: ''});
-    }
-  }
-
   render() {
     const styles = require('./RoomChat.scss');
     const { chat } = this.props;
@@ -50,19 +17,7 @@ export class RoomChat extends Component {
         </div>
 
         <RoomChatHistory />
-
-        <div className={styles.chatBox}>
-          <form onKeyDown={this.handleFormKeyDown}
-                onSubmit={this.handleSendTextMessage}>
-            <textarea
-              type="text"
-              placeholder="Type a message"
-              required
-              value={this.state.formContent}
-              onChange={this.handleFormContentChange}
-            />
-          </form>
-        </div>
+        <RoomChatMessage />
       </div>
     );
   }
@@ -70,8 +25,6 @@ export class RoomChat extends Component {
 
 RoomChat.PropTypes = {
   chat: PropTypes.element,
-
-  sendMessage: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = function mapStateToProps(state) {
@@ -80,4 +33,4 @@ const mapStateToProps = function mapStateToProps(state) {
   }
 };
 
-export default connect(mapStateToProps, { sendMessage })(RoomChat);
+export default connect(mapStateToProps)(RoomChat);
---
+++ src/containers/RoomChatMessage/index.jsx
@@ -0,0 +1,72 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import { send as sendMessage } from 'redux/modules/message';
+
+export class RoomChatMessage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formContent: '',
+    };
+
+    this.handleFormKeyDown = this.handleFormKeyDown.bind(this);
+    this.handleFormContentChange = this.handleFormContentChange.bind(this);
+    this.handleSendTextMessage = this.handleSendTextMessage.bind(this);
+  }
+
+  handleFormKeyDown(e) {
+    // Form can be submitted through the input message textarea too!
+    // That is by using the Enter key (Shift-Enter for new lines).
+    if (e.keyCode === 13 && !e.shiftKey) {
+      this.handleSendTextMessage(e);
+    }
+  }
+
+  handleFormContentChange(e) {
+    this.setState({ formContent: e.target.value });
+  }
+
+  handleSendTextMessage(e) {
+    e.preventDefault();
+
+    if (this.state.formContent !== '') {
+      this.props.sendMessage(this.props.chat._id, this.state.formContent, 'plain');
+      this.setState({formContent: ''});
+    }
+  }
+
+  render() {
+    const styles = require('./RoomChatMessage.scss');
+
+    return (
+      <div className={styles.chatBox}>
+        <form onKeyDown={this.handleFormKeyDown}
+              onSubmit={this.handleSendTextMessage}>
+            <textarea
+              type="text"
+              placeholder="Type a message"
+              required
+              value={this.state.formContent}
+              onChange={this.handleFormContentChange}
+            />
+        </form>
+      </div>
+    );
+  }
+}
+
+RoomChatMessage.PropTypes = {
+  chat: PropTypes.element,
+
+  sendMessage: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = function mapStateToProps(state) {
+  return {
+    chat: state.chat.activateResult,
+  }
+};
+
+export default connect(mapStateToProps, { sendMessage })(RoomChatMessage);
