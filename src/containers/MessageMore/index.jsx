fce2aba5b8f9e90e11dd15d065beb4687676616b
zurfyx <zurfyx@gmail.com>
2016-12-04 18:32:13 +0100

    toggle more chat

--- src/containers/MessageMore/index.jsx
+++ src/containers/MessageMore/index.jsx
@@ -2,14 +2,44 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 export class MessageMore extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handlePicture = this.handlePicture.bind(this);
+    this.handleCode = this.handleCode.bind(this);
+  }
+
+  handlePicture() {
+    alert('Coming soon...');
+  }
+
+  handleCode() {
+
+  }
+
   render() {
     const styles = require('./MessageMore.scss');
 
     return (
       <div className={styles.messageMore}>
-        <a href="#" className={styles.moreButton}>
-          <i className="fa fa-plus" aria-hidden="true"></i>
-        </a>
+        <div className={`vertical-fold ${styles.foldContainer}`}>
+          <ul>
+            <li>
+              <a className="no-decorate" onClick={this.handlePicture}>
+                <i className="fa fa-picture-o" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a className="no-decorate" onClick={this.handleCode}>
+                <i className="fa fa-code" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
+
+          <a href="#" className={styles.moreButton}>
+            <i className="fa fa-plus"></i>
+          </a>
+        </div>
       </div>
     );
   }
