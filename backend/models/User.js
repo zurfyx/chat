ee5f69101134e14397ce8574067432a46b22b5cb
zurfyx <zurfyx@gmail.com>
2017-01-28 12:38:38 +0100

    switched bcrypt-nodejs -> bcrypt

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@
  * https://github.com/sahat/hackathon-starter/blob/master/models/User.js
  */
 
-import bcrypt from 'bcrypt-nodejs';
+import bcrypt from 'bcrypt';
 import crypto from 'crypto';
 import mongoose, { Schema } from 'mongoose';
 
@@ -37,7 +37,7 @@ userSchema.pre('save', function (next) {
   if (!user.isModified('password')) { return next(); }
   bcrypt.genSalt(10, (err, salt) => {
     if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
       if (err) { return next(err); }
       user.password = hash;
       next();
