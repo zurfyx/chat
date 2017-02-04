f39a992d517c70f6d35a6d1bd031698d70b9c4be
zurfyx <zurfyx@gmail.com>
2017-02-04 23:58:40 +0100

    controllerHandler to async await

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -26,11 +26,14 @@ const router = express.Router();
  * @param promise Controller Promise.
  * @param params (req) => [params, ...].
  */
-const controllerHandler = (promise, params) => (req, res, next) => {
+const controllerHandler = (promise, params) => async (req, res, next) => {
   const boundParams = params ? params(req, res, next) : [];
-  return promise(...boundParams)
-    .then(result => res.json(result || { message: 'OK' }))
-    .catch(error => res.status(500) && next(error));
+  try {
+    const result = await promise(...boundParams);
+    return res.json(result || { message: 'OK' });
+  } catch (error) {
+    return res.status(500) && next(error);
+  }
 };
 const c = controllerHandler; // Just a name shortener.
 
