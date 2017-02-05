9a1a0a851d38e204ed61b721a4f3ecff9852e27a
zurfyx <zurfyx@gmail.com>
2017-02-06 00:23:26 +0100

    model user test -> email not empty

---
+++ backend/models/User/__tests__/index.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+
+import User from '../';
+
+describe('Model: User', () => {
+  it('should be invalid if email is empty', async () => {
+    const user = new User();
+    try {
+      await user.validate();
+    } catch (error) {
+      expect(error.errors.email).to.exist;
+    }
+  });
+});
