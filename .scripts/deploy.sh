1fa6336270286e08d1031f8de285886f6427bdd3
zurfyx <zurfyx@gmail.com>
2017-02-15 20:55:39 +0100

    deploy style

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
+
 # Upgrades server with the latest source code.
 # Requirements:
 # - A "git clone" of the repository (script will update it to the latest version)
 # - Docker & Docker Compose
 # ENV variables:
-# - $PROJECT_FOLDER: script will browse into that folder first to do a git pull, docker-compose, ...
+# - $SSH_PROJECT_FOLDER: script will browse into that folder first to do a git pull, docker-compose, ...
 # - $SSH_USERNAME
 # - $SSH_HOSTNAME
-#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -14,7 +15,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd "$DIR/.." # Go to project dir.
 
   ssh $SSH_USERNAME@$SSH_HOSTNAME -o StrictHostKeyChecking=no <<-EOF
-    cd $PROJECT_FOLDER
+    cd $SSH_PROJECT_FOLDER
     git pull
     docker-compose pull
     docker-compose stop
