fd95b4d928ea161813c4ea0ae28e4f41e0812032
zurfyx <zurfyx@gmail.com>
2017-02-19 00:56:45 +0100

    [webpack] upgraded to v2

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies, no-var, prefer-template */
+
 var webpack = require('webpack');
 var path = require('path');
 
@@ -6,100 +8,61 @@ var API_PORT = process.env.API_PORT;
 
 module.exports = {
   entry: [
-    "react-hot-loader/patch",
-    "webpack-dev-server/client?http://0.0.0.0:" + PORT,
-    "webpack/hot/only-dev-server",
-    path.join(__dirname, "..", "frontend", "app.jsx")
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://0.0.0.0:' + PORT,
+    'webpack/hot/only-dev-server',
+    path.resolve('frontend', 'app.jsx'),
   ],
   output: {
-    filename: "bundle.js",
-    publicPath: "/"
+    filename: 'bundle.js',
+    publicPath: '/',
   },
-  devtool: "#eval-source-map",
+  devtool: '#eval-source-map',
   devServer: {
     host: '0.0.0.0',
     port: PORT,
-    colors: true,
-    contentBase: path.join(__dirname, "..", "public"),
+    contentBase: path.resolve('public'),
     historyApiFallback: {
-      index: "index.html"
+      index: 'index.html',
     },
     hot: true,
     inline: false,
-    progress: true,
     proxy: {
-      "/api/": {
-        target: "http://localhost:" + API_PORT,
-        pathRewrite: {"^/api": ""},
+      '/api/': {
+        target: 'http://localhost:' + API_PORT,
+        pathRewrite: { '^/api': '' },
         changeOrigin: false,
         xfwd: true,
-      }
+      },
     },
-    stats: "errors-only"
+    stats: 'errors-only',
   },
   module: {
     loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel",
-        query: {
-          "presets": ["es2015", "es2016", "es2017", "react"],
-          "plugins": ["react-hot-loader/babel"]
-        }
-      },
-      {
-        test: /\.scss$/,
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.scss$/,
         loaders: [
-          "style-loader",
-          "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]", // TODO
-          // "css-loader?modules&importLoaders=1",
-          "sass"
-        ]
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]', // TODO
+          // 'css-loader?modules&importLoaders=1',
+          'sass-loader',
+        ],
       },
-      {
-        test: /\.css$/,
-        loaders: [
-          "style-loader",
-          // "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]--[hash:base64:5]" // TODO
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.png$/,
-        loader: "url-loader?limit=100000"
-      },
-      {
-        test: /\.jpg$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.png$/, loader: 'url-loader?limit=100000' },
+      { test: /\.jpg$/, loader: 'file-loader' },
+      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    ],
   },
   resolve: {
-    root: [
-      path.resolve('./frontend')
-    ],
-    extensions: ["", ".js", ".jsx"]
+    modules: [path.resolve('frontend'), 'node_modules'],
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
