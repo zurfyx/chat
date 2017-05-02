589d714fe372cce8b6180868a1b9e4213210ee3c
zurfyx <zurfyx@gmail.com>
2017-05-03 00:35:14 +0200

    deploy script fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     docker-compose stop
     docker-compose rm -f # Destroy outdated containers.
     docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
-    docker rm $(docker ps -a -q) # Destroy unused containers.
-    docker rmi $(docker images -q) # Destroy unused images.
+    docker rm \$(docker ps -a -q) # Destroy unused containers.
+    docker rmi \$(docker images -q) # Destroy unused images.
+    exit 0
 EOF
 )
